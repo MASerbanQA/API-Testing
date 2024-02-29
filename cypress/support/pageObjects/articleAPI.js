638ef63282a0f9c10dfc19c1d51c46faf21883ec
Alexandru Serban <mialexandru.serban@gmail.com>
2024-02-29 14:23:54 +0200

    Intercept and stub likes api test

--- cypress/support/pageObjects/articleAPI.js
+++ cypress/support/pageObjects/articleAPI.js
@@ -23,6 +23,29 @@ export class ArticleApi {
 
     }
 
+    verifyArticleLikes(){
+        cy.intercept('GET','https://conduit-api.bondaracademy.com/api/articles/feed*',{"articles":[],"articlesCount":0})
+        cy.intercept('GET','https://conduit-api.bondaracademy.com/api/articles*',{fixture:'articles.json'})
+        cy.contains('Global Feed').click()
+        cy.get('app-article-preview').find('button').then(likeButtons=>{
+            cy.wrap(likeButtons).eq(0).should('contain',300)
+            cy.wrap(likeButtons).eq(1).should('contain',99)
+        })
+
+        cy.fixture('articles.json').then(file =>{
+            const articleLink = file.articles[1].slug
+            file.articles[1].favoritesCount = 100
+            cy.intercept('POST','https://conduit-api.bondaracademy.com/api/articles/'+articleLink+'/favorite', file)
+        })
+
+        cy.get('app-article-preview button').eq(1).click().should('contain','100')
+
+
+
+
+
+    }
+
 
 }
 
